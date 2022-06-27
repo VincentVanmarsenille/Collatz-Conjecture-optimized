2f6a0da4ce7675ddd78c24674e5ab1933eca204d
VincentVanmarsenille <102739253+VincentVanmarsenille@users.noreply.github.com>
2022-06-27 08:09:26 +0200

    Update Numpy collatz eerste versie.py

--- Numpy collatz eerste versie.py
+++ Numpy collatz eerste versie.py
@@ -1,7 +1,7 @@
 import numpy as np
 from time import perf_counter
 from numba import njit
-#from functools import lru_cache
+
 
 max=1000000
 
@@ -9,9 +9,10 @@ arr = np.arange(1, max+1)
 arr=arr.astype(np.int64)
 ones=np.ones(max)
 
-#@lru_cache
+
 @njit(parallel=True)
 def Collatz_algorithm(arr):
+    arr = np.arange(1, max+1)
     while (np.array_equal(arr,ones)==False):
 
           arr = np.where((arr != 1) & (arr % 2 == 0), arr // 2, arr)
@@ -28,4 +29,5 @@ if __name__== '__main__':
     print(Collatz_algorithm(arr))  # Should return an array filled entirely with 1's.
     stop2 = perf_counter()
     print("Collatz conjecture executed in ",stop2 - start2," seconds for ",max,' iterations.')
+
 
